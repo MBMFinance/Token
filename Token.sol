214127e343697aa5e1f6164b1ec3dcbf612dee65
MBMFinance <81771025+MBMFinance@users.noreply.github.com>
2021-04-01 18:42:22 +0100

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.2;
+pragma solidity ^0.8.3;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Context.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
